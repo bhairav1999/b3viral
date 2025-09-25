a769ad65f253a2664062df6097224c8179b32d4c
Bhairav Karande <123438414+bhairav1999@users.noreply.github.com>
2025-09-25 22:18:39 +0530

    Update function.js

--- assets/js/function.js
+++ assets/js/function.js
@@ -56,34 +56,32 @@
 		},
 	});
 
-	/* testimonial Slider JS */
-	if ($('.testimonial-slider').length) {
-		const testimonial_slider = new Swiper('.testimonial-slider .swiper', {
-			slidesPerView : 1,
-			speed: 1000,
-			spaceBetween: 30,
-			loop: true,
-			autoplay: {
-				delay: 3000,
-			},
-			navigation: {
-				nextEl: '.testimonial-button-next',
-				prevEl: '.testimonial-button-prev',
-			},
-			pagination: {
-				el: '.swiper-pagination',
-				clickable: true,
-			},
-			breakpoints: {
-				768:{
-				  	slidesPerView: 2,
-				},
-				991:{
-				  	slidesPerView: 3,
-				}
-			}
-		});
-	}
+if (document.querySelector('.testimonial-slider')) {
+  new Swiper('.testimonial-slider .swiper', {
+    slidesPerView: 1,
+    speed: 1000,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+      delay: 3000,
+    },
+    navigation: {
+      nextEl: '.testimonial-button-next',
+      prevEl: '.testimonial-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    breakpoints: {
+      320: { slidesPerView: 2 },
+      380: { slidesPerView: 2 },
+      768: { slidesPerView: 2 },
+      991: { slidesPerView: 6 },
+    }
+  });
+}
+
 
 	/* Skill Bar */
 	if ($('.skills-progress-bar').length) {
